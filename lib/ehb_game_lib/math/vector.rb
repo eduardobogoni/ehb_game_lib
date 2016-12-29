f7bb67307fd8d4f079b6978a02852a78126d520f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-12-29 20:35:39 -0200

    Vector class.

---
+++ lib/ehb_game_lib/math/vector.rb
@@ -0,0 +1,52 @@
+module EhbGameLib
+  module Math
+    class Vector
+      attr_reader :x, :y
+
+      def initialize(x, y)
+        @x = x
+        @y = y
+      end
+
+      def to_s
+        "#{x}, #{y}"
+      end
+
+      def equal?(other)
+        x == other.x && y == other.y
+      end
+
+      def ==(other)
+        equal?(other)
+      end
+
+      def +(other)
+        self.class.new(x + other.x, y + other.y)
+      end
+
+      def -(other)
+        self.class.new(x - other.x, y - other.y)
+      end
+
+      def *(other)
+        self.class.new(x * other, y * other)
+      end
+
+      def /(other)
+        self.class.new(x / other, y / other)
+      end
+
+      def dot(other)
+        x * other.x + y * other.y
+      end
+
+      def length
+        @length ||= ::Math.sqrt(x * x + y * y)
+      end
+
+      def normalize
+        self / length
+      end
+    end
+  end
+end
