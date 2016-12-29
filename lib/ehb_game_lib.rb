219ee0bd42fca491961792c4f1779cfbbda9f696
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-12-29 00:02:42 -0200

    Annotate destroied objects in self.

--- lib/ehb_game_lib.rb
+++ lib/ehb_game_lib.rb
@@ -1,5 +1,6 @@
 require 'chingu'
 require 'ehb_game_lib/version'
+require 'ehb_game_lib/patches/chingu/basic_game_object'
 
 module EhbGameLib
 end
---
+++ lib/ehb_game_lib/patches/chingu/basic_game_object.rb
@@ -0,0 +1,22 @@
+module EhbGameLib
+  module Patches
+    module Chingu
+      module BasicGameObject
+        def destroy
+          super
+          @alive = false
+        end
+
+        def alive?
+          @alive.nil? ? true : false
+        end
+      end
+    end
+  end
+end
+
+unless ::Chingu::BasicGameObject.included_modules.include?(
+  ::EhbGameLib::Patches::Chingu::BasicGameObject
+)
+  ::Chingu::BasicGameObject.prepend(::EhbGameLib::Patches::Chingu::BasicGameObject)
+end
