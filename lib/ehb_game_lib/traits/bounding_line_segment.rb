3eb7168343874210fc8f357692cb792e644b975b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-12-29 22:09:50 -0200

    Bounding line segment trait.

--- lib/ehb_game_lib/traits/bounding_line_segment.rb
+++ lib/ehb_game_lib/traits/bounding_line_segment.rb
@@ -1,25 +1,55 @@
-module TopView
+module EhbGameLib
   module Traits
-    module Moveable
-      attr_accessor :speed
-
-      def setup_trait(_options)
-        super
-        @speed = 0
+    # Interface required:
+    # attr_reader :last_x, :last_y
+    module BoundingLineSegment
+      def nearest_circle_collision(klasses)
+        return [nil, nil] unless line_segment?
+        min_obj = min_p = min_dist = nil
+        each_circle_collision(klasses) do |object, p|
+          dist = Gosu.distance(last_x, last_y, p.x, p.y)
+          next unless min_dist.nil? || dist < min_dist
+          min_dist = dist
+          min_obj = object
+          min_p = p
+        end
+        [min_obj, min_p]
       end
 
-      def update_trait
-        super
-        self.x = (x + Gosu.offset_x(angle, speed))
-        self.y = (y + Gosu.offset_y(angle, speed))
+      private
+
+      def each_circle_collision(klasses)
+        each_game_object(klasses) do |object|
+          object_intersection(object).intersection.each do |p|
+            yield(object, p)
+          end
+        end
       end
 
-      def draw_trait
-        super
+      def each_game_object(klasses)
+        Array(klasses).each do |klass|
+          (klass.respond_to?(:all) ? klass.all : Array(klass)).each do |object|
+            yield(object)
+          end
+        end
       end
 
-      def speed
-        @speed
+      def line_segment?
+        last_x.nil? || last_y.nil? || x.nil? || y.nil? ? false : true
+      end
+
+      def line_segment
+        ::EhbGameLib::Math::LineSegment.new(last_x, last_y, x, y)
+      end
+
+      def object_circle(object)
+        ::EhbGameLib::Math::Circle.new(object.x, object.y, object.radius)
+      end
+
+      def object_intersection(object)
+        ::EhbGameLib::Math::Intersection::CircleLineSegment.new(
+          object_circle(object), line_segment
+        )
       end
     end
   end
