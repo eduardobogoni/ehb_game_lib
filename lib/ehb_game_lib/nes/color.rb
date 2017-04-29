e5447ab2b88b5278ba299c7d9569193af9c2ff5e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-04-29 16:43:53 -0300

    Named NES colors.

--- lib/ehb_game_lib/nes/color.rb
+++ lib/ehb_game_lib/nes/color.rb
@@ -3,6 +3,11 @@ module EhbGameLib
     class Color
       attr_accessor :color_index
 
+      BLACK = 0x0F
+      YELLOW = 0x28
+      RED = 0x05
+      WHITE = 0x30
+
       def initialize(all_colors, color_index)
         @all_colors = all_colors
         @color_index = color_index
